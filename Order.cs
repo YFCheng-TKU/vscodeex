f487515e0c79c83a8ca89ac65d52c4e60f01520a
yuanfu.c <146062@mail.tku.edu.tw>
2020-06-30 11:35:35 +0800

    work finished!

---
+++ Order.cs
@@ -0,0 +1,7 @@
+namespace vscodeex {
+    public class Order {
+        public string OrderId { get; set; }
+        public string Email { get; set; }
+        public string ProductId { get; set; }
+    }
+}
